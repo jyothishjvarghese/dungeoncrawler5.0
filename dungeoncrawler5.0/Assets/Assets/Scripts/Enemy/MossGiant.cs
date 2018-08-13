25cce1f07bec8a293857ea32428cfb5f4c3a40b6
Jyothish Varghese <jyothish.j.varghese@gmail.com>
2018-08-13 16:16:33 -0400

    Version11.3
    reverted back to the switch method instead of _currenttarget

--- dungeoncrawler5.0/Assets/Assets/Scripts/Enemy/MossGiant.cs
+++ dungeoncrawler5.0/Assets/Assets/Scripts/Enemy/MossGiant.cs
@@ -6,6 +6,7 @@ public class MossGiant : Enemy
 {
     private Vector3 _currentTarget;
     private SpriteRenderer _mossGiantEnemy;
+    private bool _switch;
 
     public void Start(){
         _mossGiantEnemy = GetComponent<SpriteRenderer>();
@@ -19,16 +20,30 @@ public class MossGiant : Enemy
     {
         if (transform.position == pointA.position)
         {
+
+            //_switch = false;
+            Debug.Log("Current target: " + _currentTarget);
             _currentTarget = pointB.position;
+            
 
         }
         else if (transform.position == pointB.position)
         {
+            //_switch = true;
+            Debug.Log("Current target: " + _currentTarget);
             _currentTarget = pointA.position;
         }
-
         transform.position = Vector3.MoveTowards(transform.position, _currentTarget, speed * Time.deltaTime);
+       
 
-        
+        //if(_switch == false)
+        //{
+        //    transform.position = Vector3.MoveTowards(transform.position, pointB.position, speed * Time.deltaTime);
+        //}
+        //if(_switch == true)
+        //{
+        //    transform.position = Vector3.MoveTowards(transform.position, pointA.position, speed * Time.deltaTime);
+        //}
     }
+    
 }
